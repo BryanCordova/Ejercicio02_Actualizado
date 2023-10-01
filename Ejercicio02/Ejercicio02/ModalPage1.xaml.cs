7b708b2dc994bd91ac0dbe3fe38441a16008b339
BryanCordova <bryan.cordova@tecsup.edu.pe>
2023-09-30 19:48:49 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio02/Ejercicio02/ModalPage1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModalPage1 : ContentPage
+    {
+        public Command BackCommand { get; set; }
+
+        public ModalPage1()
+        {
+            InitializeComponent();
+
+            BackCommand = new Command(async () =>
+            {
+                await Navigation.PopModalAsync();
+            });
+        }
+    }
+}
